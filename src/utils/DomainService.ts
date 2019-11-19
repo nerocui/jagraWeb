3fb3583e5776303d4fcd2c7c7d96e38976e82ade
nerocui <nerocui@outlook.com>
2019-11-19 01:38:04 -0800

    changed build command to git new repo structure.

--- src/utils/DomainService.ts
+++ src/utils/DomainService.ts
@@ -2,5 +2,5 @@ export function GetRootURL() {
     if (process.env.NODE_ENV === 'production') {
         return "";
     }
-    return "https://localhost:44363";
+    return "https://localhost:5001";
 }
